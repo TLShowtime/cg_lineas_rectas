846f7f9e8c15125cb5338a63ec0a69f4f5a5f3d1
Aldokler <xdavidpastor@gmail.com>
2022-03-08 18:27:31 -0600

    Algoritmos de fuerza bruta y bresenham

--- algoritmos/bresenham.c
+++ algoritmos/bresenham.c
@@ -1,9 +1,279 @@
 #include "bresenham.h"
 
 void line4(int x0, int x1, int y0, int y1){
+  int Delta_E, Delta_NE, Delta_N, Delta_NO, x, d;
+  int Delta_O, Delta_SO, Delta_S, Delta_SE, y;
 
+  d = 2*(y1-y0) - (x1-x0); //Valor que determina que pixel se debe pintar de acuerdo a su signo
+  x = x0; y = y0;
+  plot(x, y);
+
+  
+  if (x0<=x1){
+    if (y0<=y1){
+      Delta_NE  =  2 * ((y1 - y0) - (x1 - x0)); //Valor para avanzar un pixel hacia arriba y uno a la derecha
+      if (x1>=y1){ //cuadrante 1
+        Delta_E   =  2 * (y1 - y0);                 //Valor para avanzar un pixel a la derecha
+        while (x < x1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d <= 0){
+            x++;
+            d = d + Delta_E;
+          }
+          else{
+            x++; y++;
+            d = d + Delta_NE;
+          }
+          plot(x,y);
+        }
+      }
+      else{ //Cuadrante 2
+        Delta_N   = -2 * (x1 - x0);             //Valor para avanzar un pixel hacia arriba
+        while (y < y1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d >= 0){
+            y++;
+            d = d + Delta_N;
+          }
+          else{
+            x++; y++;
+            d = d + Delta_NE;
+          }
+          plot(x,y);
+        }
+      }
+    }
+    else{
+      Delta_SE  =  2 * ((y1 - y0) + (x1 - x0)); //Valor para avanzar un pixel hacia abajo y uno a la derecha
+      if (x1+y1<=y0){ //Cuadrante 7
+        Delta_S   =  2 * (x1 - x0);             //Valor para avanzar un pixel hacia abajo
+        while (y > y1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d <= 0){
+            y--;
+            d = d + Delta_S;
+          }
+          else{
+            x++; y--;
+            d = d + Delta_SE;
+          }
+          plot(x,y);
+        }
+      }
+      else{ //cuadrante 8
+        Delta_E   =  2 * (y1 - y0);                 //Valor para avanzar un pixel a la derecha
+        while (x < x1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d >= 0){
+            x++;
+            d = d + Delta_E;
+          }
+          else{
+            x++; y--;
+            d = d + Delta_SE;
+          }
+          plot(x,y);
+        }
+      }
+    }
+  }
+  else{
+    if (y0<=y1){
+      Delta_NO  = -2 * ((y1 - y0) + (x1 - x0)); //Valor para avanzar un pixel hacia arriba y uno a la izquierda
+      if (x1+y1>=x0){ //Cuadrante 3
+        Delta_N   = -2 * (x1 - x0);             //Valor para avanzar un pixel hacia arriba
+        while (y < y1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d <= 0){
+            y++;
+            d = d + Delta_N;
+          }
+          else{
+            x--; y++;
+            d = d + Delta_NO;
+          }
+          plot(x,y);
+        }
+      }    
+      else{ //Cuadrante 4
+        Delta_O   = -2 * (y1 - y0);                 //Valor para avanzar un pixel a la izquierda
+        while (x > x1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d >= 0){
+            x--;
+            d = d + Delta_O;
+          }
+          else{
+            x--; y++;
+            d = d + Delta_NO;
+          }
+          plot(x,y);
+        }
+      }
+    }
+    else{
+      Delta_SO  = -2 * ((y1 - y0) - (x1 - x0)); //Valor para avanzar un pixel hacia abajo y uno a la izquierda
+      if (x1<=y1){ //Cuadrante 5
+        Delta_O   = -2 * (y1 - y0);                 //Valor para avanzar un pixel a la izquierda
+        while (x > x1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d <= 0){
+            x--;
+            d = d + Delta_O;
+          }
+          else{
+            x--; y--;
+            d = d + Delta_SO;
+          }
+          plot(x,y);
+        }
+      }
+      else{ //Cuadrante 6
+        Delta_S   =  2 * (x1 - x0);               //Valor para avanzar un pixel hacia abajo
+        while (y > y1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d >= 0){
+            y--;
+            d = d + Delta_S;
+          }
+          else{
+            x--; y--;
+            d = d + Delta_SO;
+          }
+          plot(x,y);
+        }
+      }
+    }
+  }
 };
 
 void line4_plotless(int x0, int x1, int y0, int y1){
-    
+  int Delta_E, Delta_NE, Delta_N, Delta_NO, x, d;
+  int Delta_O, Delta_SO, Delta_S, Delta_SE, y;
+
+  d = 2*(y1-y0) - (x1-x0); //Valor que determina que pixel se debe pintar de acuerdo a su signo
+  x = x0; y = y0;
+  plot_nothing(x, y);
+
+  
+  if (x0<=x1){
+    if (y0<=y1){
+      Delta_NE  =  2 * ((y1 - y0) - (x1 - x0)); //Valor para avanzar un pixel hacia arriba y uno a la derecha
+      if (x1>=y1){ //cuadrante 1
+        Delta_E   =  2 * (y1 - y0);                 //Valor para avanzar un pixel a la derecha
+        while (x < x1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d <= 0){
+            x++;
+            d = d + Delta_E;
+          }
+          else{
+            x++; y++;
+            d = d + Delta_NE;
+          }
+          plot_nothing(x,y);
+        }
+      }
+      else{ //Cuadrante 2
+        Delta_N   = -2 * (x1 - x0);             //Valor para avanzar un pixel hacia arriba
+        while (y < y1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d >= 0){
+            y++;
+            d = d + Delta_N;
+          }
+          else{
+            x++; y++;
+            d = d + Delta_NE;
+          }
+          plot_nothing(x,y);
+        }
+      }
+    }
+    else{
+      Delta_SE  =  2 * ((y1 - y0) + (x1 - x0)); //Valor para avanzar un pixel hacia abajo y uno a la derecha
+      if (x1+y1<=y0){ //Cuadrante 7
+        Delta_S   =  2 * (x1 - x0);             //Valor para avanzar un pixel hacia abajo
+        while (y > y1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d <= 0){
+            y--;
+            d = d + Delta_S;
+          }
+          else{
+            x++; y--;
+            d = d + Delta_SE;
+          }
+          plot_nothing(x,y);
+        }
+      }
+      else{ //cuadrante 8
+        Delta_E   =  2 * (y1 - y0);                 //Valor para avanzar un pixel a la derecha
+        while (x < x1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d >= 0){
+            x++;
+            d = d + Delta_E;
+          }
+          else{
+            x++; y--;
+            d = d + Delta_SE;
+          }
+          plot_nothing(x,y);
+        }
+      }
+    }
+  }
+  else{
+    if (y0<=y1){
+      Delta_NO  = -2 * ((y1 - y0) + (x1 - x0)); //Valor para avanzar un pixel hacia arriba y uno a la izquierda
+      if (x1+y1>=x0){ //Cuadrante 3
+        Delta_N   = -2 * (x1 - x0);             //Valor para avanzar un pixel hacia arriba
+        while (y < y1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d <= 0){
+            y++;
+            d = d + Delta_N;
+          }
+          else{
+            x--; y++;
+            d = d + Delta_NO;
+          }
+          plot_nothing(x,y);
+        }
+      }    
+      else{ //Cuadrante 4
+        Delta_O   = -2 * (y1 - y0);                 //Valor para avanzar un pixel a la izquierda
+        while (x > x1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d >= 0){
+            x--;
+            d = d + Delta_O;
+          }
+          else{
+            x--; y++;
+            d = d + Delta_NO;
+          }
+          plot_nothing(x,y);
+        }
+      }
+    }
+    else{
+      Delta_SO  = -2 * ((y1 - y0) - (x1 - x0)); //Valor para avanzar un pixel hacia abajo y uno a la izquierda
+      if (x1<=y1){ //Cuadrante 5
+        Delta_O   = -2 * (y1 - y0);                 //Valor para avanzar un pixel a la izquierda
+        while (x > x1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d <= 0){
+            x--;
+            d = d + Delta_O;
+          }
+          else{
+            x--; y--;
+            d = d + Delta_SO;
+          }
+          plot_nothing(x,y);
+        }
+      }
+      else{ //Cuadrante 6
+        Delta_S   =  2 * (x1 - x0);               //Valor para avanzar un pixel hacia abajo
+        while (y > y1){ //escoge cual pixel pintar de acuerdo al signo de "d" hasta terminar la linea
+          if (d >= 0){
+            y--;
+            d = d + Delta_S;
+          }
+          else{
+            x--; y--;
+            d = d + Delta_SO;
+          }
+          plot_nothing(x,y);
+        }
+      }
+    }
+  }
 };
--- algoritmos/bruta.c
+++ algoritmos/bruta.c
@@ -1,8 +1,87 @@
 #include "bruta.h"
 
 void line1(int x0, int x1, int y0, int y1){
+  long double b, m, y, x, my, mx;
+  int i;
+
+  my = (y1 - y0); //Se usa para evitar problemas de punto flotante
+  mx = (x1 - x0); //Se usa para evitar problemas de punto flotante
+  m = my/mx;
+  b = y0 - m*x0;
+  x = x1-x0; x = fabs(x); 
+  y = y1-y0; y = fabs(y); 
+
+  if (x>=y){  //Se realizan comparaciones para averiguar sobre cual eje ir pintando y en que dirección pintar
+    if (x0 > x1){
+      for (i = x1; i<=x0; i++){ //Pinta hacia la izquierda usando el eje x como base
+        y = m*i + b;            //Se utiliza la fórmula para encontrar y
+        plot(i, round(y));
+      }
+    }
+    else{
+      for (i = x0; i<=x1; i++){ //Pinta hacia la derecha usando el eje x como base
+        y = m*i + b;            //Se utiliza la fórmula para encontrar y
+        plot(i, round(y));
+      }
+    }
+  }
+  else{
+    if (y0 > y1){
+      for (i = y1; i<=y0; i++){ //Pinta hacia la izquierda usando el eje y como base
+        y = (i - b); //Se usa para evitar problemas de punto flotante
+        x = y/m;                //Se utiliza la fórmula para encontrar x
+        plot(round(x), i);
+      }
+    }
+    else{
+      for (i = y0; i<=y1; i++){ //Pinta hacia la derecha usando el eje y como base
+        y = (i - b); //Se usa para evitar problemas de punto flotante
+        x = y/m;                //Se utiliza la fórmula para encontrar x
+        plot(round(x), i);
+      }
+    }
+  }
 };
 
 void line1_plotless(int x0, int x1, int y0, int y1){
+  long double b, m, y, x, my, mx;
+  int i;
 
+  my = (y1 - y0); //Se usa para evitar problemas de punto flotante
+  mx = (x1 - x0); //Se usa para evitar problemas de punto flotante
+  m = my/mx;
+  b = y0 - m*x0;
+  x = x1-x0; x = fabs(x); 
+  y = y1-y0; y = fabs(y); 
+
+  if (x>=y){  //Se realizan comparaciones para averiguar sobre cual eje ir pintando y en que dirección pintar
+    if (x0 > x1){
+      for (i = x1; i<=x0; i++){ //Pinta hacia la izquierda usando el eje x como base
+        y = m*i + b;            //Se utiliza la fórmula para encontrar y
+        plot_nothing(i, round(y));
+      }
+    }
+    else{
+      for (i = x0; i<=x1; i++){ //Pinta hacia la derecha usando el eje x como base
+        y = m*i + b;            //Se utiliza la fórmula para encontrar y
+        plot_nothing(i, round(y));
+      }
+    }
+  }
+  else{
+    if (y0 > y1){
+      for (i = y1; i<=y0; i++){ //Pinta hacia la izquierda usando el eje y como base
+        y = (i - b); //Se usa para evitar problemas de punto flotante
+        x = y/m;                //Se utiliza la fórmula para encontrar x
+        plot_nothing(round(x), i);
+      }
+    }
+    else{
+      for (i = y0; i<=y1; i++){ //Pinta hacia la derecha usando el eje y como base
+        y = (i - b); //Se usa para evitar problemas de punto flotante
+        x = y/m;                //Se utiliza la fórmula para encontrar x
+        plot_nothing(round(x), i);
+      }
+    }
+  }
 };
